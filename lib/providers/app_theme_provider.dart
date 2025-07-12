1ab55ae6c8006b0a3b77321e4ec3642ec74b72a4
aliaaemad312 <aliaa.emad312@gmail.com>
2025-07-12 03:06:06 +0300

    + Added shared Preference for  both localization and theming.

--- lib/providers/app_theme_provider.dart
+++ lib/providers/app_theme_provider.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppThemeProvider extends ChangeNotifier{
+  static const String themeKey='app_theme';
   ThemeMode appTheme=ThemeMode.light;
 
-  void changeTheme(ThemeMode newTheme){
+  AppThemeProvider(){
+    loadSavedTheme();
+  }
+
+  void loadSavedTheme ()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? savedTheme=prefs.getBool(AppThemeProvider.themeKey);
+    if(savedTheme==isDarkMode()){
+      appTheme=ThemeMode.dark;
+    }else{
+      appTheme=ThemeMode.light;
+    }
+    notifyListeners();
+
+
+  }
+
+  void changeTheme(ThemeMode newTheme)async{
     if(appTheme==newTheme){
       return;
     }
     appTheme=newTheme;
     notifyListeners();
+    
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(AppThemeProvider.themeKey, appTheme==ThemeMode.light?true:false);
+    
+    
+    
   }
 
   bool isDarkMode(){
